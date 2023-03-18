c3da652dbf4876cb31ef88d004f8d9fe38705d15
Klimch1k <114351322+Klimch1k@users.noreply.github.com>
2023-03-19 00:26:00 +0200

    Task -2 without CSS

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,16 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.6.min.css';
 
+const startButton = document.querySelector('button[data-start]');
+startButton.disabled = true;
+const daysEl = document.querySelector('span[data-days]');
+const hoursEl = document.querySelector('span[data-hours]');
+const minutesEl = document.querySelector('span[data-minutes]');
+const secondsEl = document.querySelector('span[data-seconds]');
+
+let startButtonIsActive = false;
 let userDate = null;
 const options = {
   enableTime: true,
@@ -10,7 +20,8 @@ const options = {
   onClose(selectedDates) {
     userDate = selectedDates[0].getTime();
     if (selectedDates[0].getTime() < Date.now()) {
-      window.alert('Please choose a date in the future');
+      Notiflix.Notify.failure("Please choose a date in the future");
+      return;
     } else {
       startButton.disabled = false;
     }
@@ -19,15 +30,27 @@ const options = {
 
 flatpickr('input[type="text"]', options);
 
-const startButton = document.querySelector('button[data-start]');
-startButton.disabled = true;
 startButton.addEventListener('click', onStartButtonClick);
 
 function onStartButtonClick() {
- setInterval(() => {
+  if (startButtonIsActive) {
+    return;
+  }
+  startButtonIsActive = true;
+  let timerId = setInterval(() => {
     const currentTime = Date.now();
     const deltaTime = userDate - currentTime;
+    const timeElements = convertMs(deltaTime);
+    updateTimer(timeElements);
     
+    if (currentTime >= userDate) {
+      stopTimer(timerId);
+      daysEl.textContent = '00';
+      hoursEl.textContent = '00';
+      minutesEl.textContent = '00';
+      secondsEl.textContent = '00';
+
+    }
   }, 1000);
 }
 
@@ -37,10 +60,27 @@ function convertMs(ms) {
   const hour = minute * 60;
   const day = hour * 24;
 
-  const days = Math.floor(ms / day);
-  const hours = Math.floor((ms % day) / hour);
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const days = pad(Math.floor(ms / day));
+  const hours = pad(Math.floor((ms % day) / hour));
+  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
+  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
 
   return { days, hours, minutes, seconds };
+}
+
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
+
+function updateTimer(obj) {
+  daysEl.textContent = obj.days;
+  hoursEl.textContent = obj.hours;
+  minutesEl.textContent = obj.minutes;
+  secondsEl.textContent = obj.seconds;
+}
+
+
+function stopTimer(value) {
+  clearInterval(value);
+
 }
