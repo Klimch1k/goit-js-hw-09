65f1c45f1118afeb4087ca46c3cf7b9b75963875
Klimch1k <114351322+Klimch1k@users.noreply.github.com>
2023-03-19 18:28:59 +0200

    Task - 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.6.min.css';
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', handleFormSubmit);
+
+function handleFormSubmit(e) {
+  e.preventDefault();
+  const {
+    elements: { delay, step, amount },
+  } = e.currentTarget;
+  let s = 0;
+  for (let i = 1; i <= amount.value; i++) {
+    s += parseInt(step.value);
+    createPromise(i, s)
+      .then(({ position, delay }) => {
+        console.log();
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  }
+  e.currentTarget.reset();
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
